f5cab5241c193014aed0947afe587273489542b6
enuu <enuu9923@gmail.com>
2025-07-29 17:22:27 +0900

    0729

--- script/script.js
+++ script/script.js
@@ -1,7 +1,27 @@
 
 
 document.addEventListener("DOMContentLoaded", function(e){
-
+    rightMenuSlide();
 })
 
 
+const rightMenuSlide =() => {
+    $('header .header_div .ham_btn').click(function(){
+        $('.side_menu').addClass('on')
+        // $('.sub_menu_dim').addClass('on')
+    })
+
+    $('.side_menu .close_btn').click(function(){
+        $('.side_menu').removeClass('on')
+    })
+
+    // $('.sub_menu_dim').click(function(){
+    //     $('.sub_menu').removeClass('on')
+    //     $('.sub_menu_dim').removeClass('on')
+    // })
+
+    // $('.sub_menu .sub_div .menu_close').click(function(){
+    //     $('.sub_menu').removeClass('on')
+    //     $('.sub_menu_dim').removeClass('on')
+    // })
+}
