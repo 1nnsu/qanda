e15a4e88d31ccb381d6eacd7203e652118d092f4
enuu <enuu9923@gmail.com>
2025-07-30 13:59:32 +0900

    0730

--- script/script.js
+++ script/script.js
@@ -2,26 +2,37 @@
 
 document.addEventListener("DOMContentLoaded", function(e){
     rightMenuSlide();
+    resizeRightMenuSlide();
 })
 
 
 const rightMenuSlide =() => {
     $('header .header_div .ham_btn').click(function(){
         $('.side_menu').addClass('on')
-        // $('.sub_menu_dim').addClass('on')
+        $('.side_dim').addClass('on')
+    })
+
+
+    $('.side_dim').click(function(){
+        $('.side_menu').removeClass('on')
+        $('.side_dim').removeClass('on')
     })
 
     $('.side_menu .close_btn').click(function(){
         $('.side_menu').removeClass('on')
+        $('.side_dim').removeClass('on')
     })
+}
 
-    // $('.sub_menu_dim').click(function(){
-    //     $('.sub_menu').removeClass('on')
-    //     $('.sub_menu_dim').removeClass('on')
-    // })
-
-    // $('.sub_menu .sub_div .menu_close').click(function(){
-    //     $('.sub_menu').removeClass('on')
-    //     $('.sub_menu_dim').removeClass('on')
-    // })
+const resizeRightMenuSlide =() => {
+    // 윈도우가 리사이즈 될때 반응형 !
+    $(window).resize(function() {
+        if($(window).width() < 800) {
+            // window 크기가 800보다 작을때
+        } else {
+            // window 크기가 800보다 클때
+            $('.side_menu').removeClass('on')
+            $('.side_dim').removeClass('on')
+        }
+    })
 }
